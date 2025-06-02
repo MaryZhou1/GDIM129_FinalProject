0d478843ae41fa776697aefcb49a310bd0f69807
Mary Zhou <xingmeiz@uci.edu>
2025-06-01 17:14:14 -0700

    x

--- FinalProjectUnity/Assets/InGameUI.cs
+++ FinalProjectUnity/Assets/InGameUI.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // ------------------- Sanity -------------------
+    //public TMP_Text SanTxt;
+    //public Image SanBar;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //public void SanityUI(int sanity, int max_san, float initialWidth)
+    //{
+    //    SanTxt.text = sanity.ToString();
+    //    float fillAmount = (float)sanity / (float)max_san;
+    //    SanBar.rectTransform.sizeDelta = new Vector2(initialWidth * fillAmount, SanBar.rectTransform.sizeDelta.y);
+    //}
+
+
+    // ------------------- Buttons -------------------
 
     public void HomeButton()
     {
