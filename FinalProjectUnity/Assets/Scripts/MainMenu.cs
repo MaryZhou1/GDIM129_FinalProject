bd90f8321e43c40eb04eb0bc03f5dd660447900e
DwdNmsj4 <jingwed4@uci.edu>
2025-06-05 15:00:36 +0800

    add tutorial panel

--- FinalProjectUnity/Assets/Scripts/MainMenu.cs
+++ FinalProjectUnity/Assets/Scripts/MainMenu.cs
@@ -3,9 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject tutorialPanel; // Tutorial UI 面板
+    public GameObject mainMenuPanel; // 主菜单面板（可选）
+
     public void StartGame()
     {
-        SceneManager.LoadScene("PrologueScene"); 
+        SceneManager.LoadScene("PrologueScene");
+    }
+
+    public void OpenTutorial()
+    {
+        tutorialPanel.SetActive(true);
+        if (mainMenuPanel != null) mainMenuPanel.SetActive(false);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorialPanel.SetActive(false);
+        if (mainMenuPanel != null) mainMenuPanel.SetActive(true);
     }
 
     public void QuitGame()
