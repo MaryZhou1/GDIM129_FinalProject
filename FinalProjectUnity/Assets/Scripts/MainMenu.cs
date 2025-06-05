b940f5fb39e1bfda1c962a2fb7a791a8d804a8e1
Mary Zhou <xingmeiz@uci.edu>
2025-06-05 14:35:42 -0700

    update

--- FinalProjectUnity/Assets/Scripts/MainMenu.cs
+++ FinalProjectUnity/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip clip;
+
+
     public GameObject tutorialPanel; // Tutorial UI 面板
     public GameObject mainMenuPanel; // 主菜单面板（可选）
 
@@ -13,12 +17,16 @@ public class MainMenu : MonoBehaviour
 
     public void OpenTutorial()
     {
+        source.PlayOneShot(clip);
+
         tutorialPanel.SetActive(true);
         if (mainMenuPanel != null) mainMenuPanel.SetActive(false);
     }
 
     public void CloseTutorial()
     {
+        source.PlayOneShot(clip);
+
         tutorialPanel.SetActive(false);
         if (mainMenuPanel != null) mainMenuPanel.SetActive(true);
     }
